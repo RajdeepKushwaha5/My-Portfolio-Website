b32d2bf561b6d7ecb63d8f596e6d8c733559b83d
Rajdeep Singh <157990661+RajdeepKushwaha5@users.noreply.github.com>
2025-07-18 20:42:35 +0530

    Update About.tsx

--- components/About.tsx
+++ components/About.tsx
@@ -40,7 +40,7 @@ export const About: React.FC = () => {
                          <div className="absolute inset-0 bg-gradient-to-br from-sky-400 to-purple-600 rounded-full blur-xl opacity-50"></div>
                          <img
                             src="https://www.shutterstock.com/image-vector/teenager-boy-programmer-editing-digital-code-vector-2496933763"
-                            alt="A male anime character representing Rajdeep Singh Kushwaha"
+                            alt="https://i.ibb.co/d5tC2B7/anime-pfp.jpg"
                             className="relative w-full h-full object-cover rounded-full shadow-2xl border-4 border-white dark:border-slate-800"
                         />
                     </div>
