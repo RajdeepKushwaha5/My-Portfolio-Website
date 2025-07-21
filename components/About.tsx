c9da95c15e485cd5c3eef8363ced636c1f90fb78
Rajdeep Singh <157990661+RajdeepKushwaha5@users.noreply.github.com>
2025-07-21 14:06:49 +0530

    Update About.tsx

--- components/About.tsx
+++ components/About.tsx
@@ -39,8 +39,8 @@ export const About: React.FC = () => {
                     <div className="relative w-64 h-64">
                          <div className="absolute inset-0 bg-gradient-to-br from-sky-400 to-purple-600 rounded-full blur-xl opacity-50"></div>
                          <img
-                            src="https://www.shutterstock.com/image-vector/teenager-boy-programmer-editing-digital-code-vector-2496933763"
-                            alt="https://i.ibb.co/d5tC2B7/anime-pfp.jpg"
+                            src="https://drive.google.com/file/d/1NtKBNnBsT9vFcmBBO-NjWoy77SqiOSnh/view?usp=drive_link"
+                            alt="Rajdeep Singh Kushwaha"
                             className="relative w-full h-full object-cover rounded-full shadow-2xl border-4 border-white dark:border-slate-800"
                         />
                     </div>
