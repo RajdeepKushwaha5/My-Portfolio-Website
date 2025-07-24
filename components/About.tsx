90983790562b4d22a22c7610c8592e1298a655a3
Rajdeep Singh <157990661+RajdeepKushwaha5@users.noreply.github.com>
2025-07-25 01:01:54 +0530

    Update About.tsx

--- components/About.tsx
+++ components/About.tsx
@@ -39,7 +39,7 @@ export const About: React.FC = () => {
                     <div className="relative w-64 h-64">
                          <div className="absolute inset-0 bg-gradient-to-br from-sky-400 to-purple-600 rounded-full blur-xl opacity-50"></div>
                         <img
-  src="https://drive.google.com/file/d/1HSRWsj--WK1APvShOPOq7ZLLMC1qeqPw/view?usp=drive_link"
+  src="components/my_image_ghibli_2.jpg"
   alt="Rajdeep Singh Kushwaha"
   className="relative w-full h-full object-cover rounded-full shadow-2xl border-4 border-white dark:border-slate-800"
 />
