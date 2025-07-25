e4a5049c484fe809f6271839b4a3ce6600a39da7
Rajdeep Singh <157990661+RajdeepKushwaha5@users.noreply.github.com>
2025-07-25 07:53:26 +0530

    Update About.tsx

--- components/About.tsx
+++ components/About.tsx
@@ -39,11 +39,10 @@ export const About: React.FC = () => {
                     <div className="relative w-64 h-64">
                          <div className="absolute inset-0 bg-gradient-to-br from-sky-400 to-purple-600 rounded-full blur-xl opacity-50"></div>
 <img
-  src=""
-  alt=""
-  className=""
+  src="/filnal_image.png"
+  alt="Rajdeep Singh"
+  className="relative w-full h-full object-cover rounded-full shadow-2xl border-4 border-white dark:border-slate-800"
 />
-
 
 
                     </div>
